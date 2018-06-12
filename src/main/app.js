ed95bf3dc39de7e8feb6d78d856ac9dbcdca87b6
Ameeen Ramadan-Jradi <ameen.ramadan.jradi@gmail.com>
2018-06-12 18:33:28 +1000

    Add home routing view

--- src/main/app.js
+++ src/main/app.js
@@ -1,19 +1,30 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router'])
+    .config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                });
 
-app.factory('posts', [function() {
-    var o = {
-        posts: [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ]
-    };
-    return o;
-}]);
-
-app.controller('MainCtrl', [
+            $urlRouterProvider.otherwise('home');
+    }])
+    .factory('posts', [function() {
+        var o = {
+            posts: [
+                {title: 'post 1', upvotes: 5},
+                {title: 'post 2', upvotes: 2},
+                {title: 'post 3', upvotes: 15},
+                {title: 'post 4', upvotes: 9},
+                {title: 'post 5', upvotes: 4}
+            ]
+        };
+        return o;
+    }])
+    .controller('MainCtrl', [
     '$scope',
     'posts',
     function($scope, posts) {
