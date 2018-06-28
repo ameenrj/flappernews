080dbc536eff9bfce8c3d844617b36009b1b674c
Ameeen Ramadan-Jradi <ameen.ramadan.jradi@gmail.com>
2018-06-28 18:05:31 +1000

    Secure endpoints with JWT authentication

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -7,8 +7,48 @@ router.get('/', function(req, res, next) {
 });
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+var jwt = require('express-jwt');
+
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+
+router.post('register', function(req, res, next) {
+   if (!req.body.username || !req.body.password) {
+       return res.status(400).json({message: 'Please fill out all fields'});
+   }
+
+   var user = new User();
+
+   user.username = req.body.username;
+   user.password = user.setPassword(req.body.password);
+
+   user.save(function(err) {
+       if (err) { return next(err); }
+
+       return res.json({token: user.generateJWT()})
+   });
+});
+
+router.post('login', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+
+        if (user) {
+            return res.json({token: user.generateJWT()})
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
+});
 
 router.param('post', function(req, res, next, id) {
     var query = Post.findById(id);
@@ -50,8 +90,9 @@ router.get('/posts/:post', function (req, res) {
     });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
     var post = new Post(req.body);
+    post.author = req.payload.username;
 
     post.save(function(err, post) {
         if (err){ return next(err); }
@@ -60,7 +101,7 @@ router.post('/posts', function(req, res, next) {
     });
 });
 
-router.put('/posts/:post/upvote', function (req, res, next) {
+router.put('/posts/:post/upvote', auth, function (req, res, next) {
     req.post.upvote(function(err, post) {
         if (err){ return next(err); }
 
@@ -68,9 +109,10 @@ router.put('/posts/:post/upvote', function (req, res, next) {
     });
 });
 
-router.post('/posts/:post/comments', function (req, res, next) {
+router.post('/posts/:post/comments', auth, function (req, res, next) {
     var comment = new Comment(req.body);
     comment.post = req.post;
+    comment.author = req.payload.username;
 
     comment.save(function(err, comment) {
         if (err){ return next(err); }
@@ -84,7 +126,7 @@ router.post('/posts/:post/comments', function (req, res, next) {
     });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function (req, res, next) {
     req.comment.upvote(function(err, comment) {
         if (err){ return next(err); }
 
